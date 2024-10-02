2fcf1ce6cc41c5611f34b6253d4cb79cea828bb8
Youssef Sobhy <youssefsobhymo7amed@gmail.com>
2024-10-02 08:29:47 +0300

    Update seed.py

--- SEED/seed.py
+++ SEED/seed.py
@@ -27,7 +27,7 @@ print("""\033[1;37m
 ╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗                    
 ║\33[0;41m[ ENTER THE TOOL'S PASSWORD ✅ ] \033[0;92m║
 ╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝              """)
-password = 'YOUSSEF'
+password = 'ELJOOYOUSSEF'
 one = str(input('''❖ - 𝐏𝐀𝐒𝐒𝐖𝐑𝐃 :  ''') )
 if one == password:
     print(f"""
